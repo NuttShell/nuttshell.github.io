39e95841fe0743e822d8071146002e67f06174b5
Max NuttShell <NuttShell@users.noreply.github.com>
2026-08-27 13:14:56 +0700

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -16,9 +16,6 @@ param(
     [switch]$NoWait
 )
 
-# TLS 1.2/1.3 нужно включить ДО скачивания самого этого файла, а не внутри get_con.cmd -
-# иначе PowerShell 5.1 на многих машинах по умолчанию не сможет установить соединение с
-# GitHub Pages и упадёт с "The underlying connection was closed" ещё на этом irm-вызове.
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 -bor [Net.SecurityProtocolType]::Tls13
 
 $CON_CMD_URL = "https://raw.githubusercontent.com/NuttShell/localpython_installer/main/get_con.cmd"
